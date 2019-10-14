9e1b4f8d23422435d95920e2083b59ac580381e8
블루 <chlwjdtn1120@gmail.com>
2019-10-14 13:02:10 +0900

    check_valid_addr added (sc-bad-sp, arg solved)

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,12 +1,16 @@
 #include "userprog/syscall.h"
+#include "userprog/pagedir.h"
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 #include "devices/shutdown.h"
 #include "devices/input.h"
 
 static void syscall_handler (struct intr_frame *);
+void check_valid_addr (const void *vaddr);
+void exit (int status);
 int read (int fd, void *buffer, unsigned size);
 int write (int fd, const void *buffer, unsigned size);
 void
@@ -18,53 +22,65 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f) 
 {
+  check_valid_addr ((uint32_t *)(f->esp));
   int sysnum = *(int *)f->esp;
   // printf ("system call %d!\n", sysnum);
 
   switch (sysnum)
   {
-  case SYS_HALT:
+  case SYS_HALT: // 0
     shutdown_power_off ();
     break;
   
-  case SYS_EXIT:
-    printf ("%s: exit(%d)\n", thread_name (), *(int *)(f->esp + 4));
-    thread_exit ();
+  case SYS_EXIT: // 1
+    check_valid_addr ((uint32_t *)(f->esp + 4));
+    exit (*(int *)(f->esp + 4));
     break;
   
   case SYS_EXEC: // 1
+    check_valid_addr ((uint32_t *)(f->esp + 4));
     break;
   
   case SYS_WAIT: // 1
+    check_valid_addr ((uint32_t *)(f->esp + 4));
     break;
   
   case SYS_CREATE: // 2
+    check_valid_addr ((uint32_t *)(f->esp + 8));
     break;
   
   case SYS_REMOVE: // 1
+    check_valid_addr ((uint32_t *)(f->esp + 4));
     break;
 
   case SYS_OPEN: // 1
+    check_valid_addr ((uint32_t *)(f->esp + 4));
     break;
 
   case SYS_FILESIZE: // 1
+    check_valid_addr ((uint32_t *)(f->esp + 4));
     break;
 
   case SYS_READ: // 3
+    check_valid_addr ((uint32_t *)(f->esp + 12));
     f->eax = read ((int)*(uint32_t *)(f->esp + 4), (void *)*(uint32_t *)(f->esp + 8), (unsigned)*(uint32_t *)(f->esp + 12));
     break;
 
   case SYS_WRITE: // 3
+    check_valid_addr ((uint32_t *)(f->esp + 12));
     f->eax = write ((int)*(uint32_t *)(f->esp + 4), (void *)*(uint32_t *)(f->esp + 8), (unsigned)*(uint32_t *)(f->esp + 12));
     break;
 
   case SYS_SEEK: // 2
+    check_valid_addr ((uint32_t *)(f->esp + 8));
     break;
 
   case SYS_TELL: // 1
+    check_valid_addr ((uint32_t *)(f->esp + 4));
     break;
 
   case SYS_CLOSE: // 1
+    check_valid_addr ((uint32_t *)(f->esp + 4));
     break;
   
   default:
@@ -72,6 +88,16 @@ syscall_handler (struct intr_frame *f)
   }
 
   // thread_exit ();
+}
+
+void check_valid_addr (const void *vaddr) {
+  if (is_user_vaddr (vaddr) == false || !pagedir_get_page (thread_current ()->pagedir, vaddr))
+    exit (-1);
+}
+
+void exit (int status) {
+  printf ("%s: exit(%d)\n", thread_name (), status);
+  thread_exit ();
 }
 
 int read (int fd, void *buffer UNUSED, unsigned size) {
