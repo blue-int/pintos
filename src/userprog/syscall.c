d43c9d0db22a152c87d060fd2ee99366fec8bb62
블루 <chlwjdtn1120@gmail.com>
2019-10-14 17:07:13 +0900

    create-* solved

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -7,12 +7,28 @@
 #include "threads/vaddr.h"
 #include "devices/shutdown.h"
 #include "devices/input.h"
+#include "filesys/filesys.h"
 
 static void syscall_handler (struct intr_frame *);
 void check_valid_addr (const void *vaddr);
+
+typedef int pid_t;
+#define PID_ERROR ((pid_t) -1)
+
+void halt (void);
 void exit (int status);
+pid_t exec (const char *cmd_line);
+int wait (pid_t pid);
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
 int read (int fd, void *buffer, unsigned size);
 int write (int fd, const void *buffer, unsigned size);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
+
 void
 syscall_init (void) 
 {
@@ -22,65 +38,71 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f) 
 {
-  check_valid_addr ((uint32_t *)(f->esp));
+  // hex_dump ((uintptr_t)f->esp, f->esp, 0xc0000000 - (uintptr_t)f->esp, true);
+  uint32_t *esp = (uint32_t *)(f->esp);
+  check_valid_addr (esp);
   int sysnum = *(int *)f->esp;
+  uint32_t *arg0 = (uint32_t *)(f->esp + 4);
+  uint32_t *arg1 = (uint32_t *)(f->esp + 8);
+  uint32_t *arg2 = (uint32_t *)(f->esp + 12);
   // printf ("system call %d!\n", sysnum);
 
   switch (sysnum)
   {
   case SYS_HALT: // 0
-    shutdown_power_off ();
+    halt ();
     break;
   
   case SYS_EXIT: // 1
-    check_valid_addr ((uint32_t *)(f->esp + 4));
+    check_valid_addr (arg0);
     exit (*(int *)(f->esp + 4));
     break;
   
   case SYS_EXEC: // 1
-    check_valid_addr ((uint32_t *)(f->esp + 4));
+    check_valid_addr (arg0);
     break;
   
   case SYS_WAIT: // 1
-    check_valid_addr ((uint32_t *)(f->esp + 4));
+    check_valid_addr (arg0);
     break;
   
   case SYS_CREATE: // 2
-    check_valid_addr ((uint32_t *)(f->esp + 8));
+    check_valid_addr (arg1);
+    f->eax = create ((const char *)*arg0, (unsigned)*arg1);
     break;
   
   case SYS_REMOVE: // 1
-    check_valid_addr ((uint32_t *)(f->esp + 4));
+    check_valid_addr (arg0);
     break;
 
   case SYS_OPEN: // 1
-    check_valid_addr ((uint32_t *)(f->esp + 4));
+    check_valid_addr (arg0);
     break;
 
   case SYS_FILESIZE: // 1
-    check_valid_addr ((uint32_t *)(f->esp + 4));
+    check_valid_addr (arg0);
     break;
 
   case SYS_READ: // 3
-    check_valid_addr ((uint32_t *)(f->esp + 12));
-    f->eax = read ((int)*(uint32_t *)(f->esp + 4), (void *)*(uint32_t *)(f->esp + 8), (unsigned)*(uint32_t *)(f->esp + 12));
+    check_valid_addr (arg2);
+    f->eax = read ((int)*arg0, (void *)*arg1, (unsigned)*arg2);
     break;
 
   case SYS_WRITE: // 3
-    check_valid_addr ((uint32_t *)(f->esp + 12));
-    f->eax = write ((int)*(uint32_t *)(f->esp + 4), (void *)*(uint32_t *)(f->esp + 8), (unsigned)*(uint32_t *)(f->esp + 12));
+    check_valid_addr (arg2);
+    f->eax = write ((int)*arg0, (void *)*arg1, (unsigned)*arg2);
     break;
 
   case SYS_SEEK: // 2
-    check_valid_addr ((uint32_t *)(f->esp + 8));
+    check_valid_addr (arg1);
     break;
 
   case SYS_TELL: // 1
-    check_valid_addr ((uint32_t *)(f->esp + 4));
+    check_valid_addr (arg0);
     break;
 
   case SYS_CLOSE: // 1
-    check_valid_addr ((uint32_t *)(f->esp + 4));
+    check_valid_addr (arg0);
     break;
   
   default:
@@ -95,10 +117,19 @@ void check_valid_addr (const void *vaddr) {
     exit (-1);
 }
 
+void halt (void) {
+  shutdown_power_off ();
+};
+
 void exit (int status) {
   printf ("%s: exit(%d)\n", thread_name (), status);
   thread_exit ();
 }
+
+bool create (const char *file, unsigned initial_size) {
+  check_valid_addr (file);
+  return filesys_create (file, (off_t)initial_size);
+};
 
 int read (int fd, void *buffer UNUSED, unsigned size) {
   if (fd == 0) {
